26ff0d8d19fed9c60b56806fc3ddf92d2cd5ad6e
kik557473@gmail.com <kik557473@gmail.com>
2021-02-21 15:29:52 +0000

    05 create login system

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,13 +20,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   def destroy
     super
+  end
+  
+  def verify
   end
 
   # GET /resource/cancel
@@ -42,12 +45,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :postal_code, :address, :phone, :email, :password, :password_confirmation ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :postal_code, :address, :phone, :email, :password, :password_confirmation ])
   end
 
   # The path used after sign up.
@@ -56,7 +59,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    verify_path
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -17,11 +17,19 @@ class Users::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+  
+  def after_sign_in_path_for(user)
+    products_path
+  end
+
+  def after_sign_out_path_for(user)
+    root_path
+  end
 
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :name, :postal_code, :address, :phone, :email, :password, :password_confirmation ])
   end
 end
