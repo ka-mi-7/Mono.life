0c9fd6bb5c7d3b24ac6a3e6b6301cc38ce579df3
kik557473@gmail.com <kik557473@gmail.com>
2021-03-10 06:22:42 +0000

    create shopping_cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -25,6 +25,9 @@ class Product < ApplicationRecord
       }
     }
   
+  scope :in_cart_product_names, -> (cart_item_ids) { where(id: cart_item_ids).pluck(:name) }
+
+  
   def reviews_new
     reviews.new
   end
